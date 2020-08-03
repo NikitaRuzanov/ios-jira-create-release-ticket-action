c3560fd281c6d681d5dd379aa16c2458a2033c8d
Nikita Ruzanov <nikita.ruzanov@bolt.eu>
2020-08-03 12:13:12 +0300

    --wip-- [skip ci]

--- index.ts
+++ index.ts
@@ -59,7 +59,7 @@ async function main() {
         }
       }
     })
-    await jira_test.issues.getCreateIssueMetadata({projectIds: [inputs.jiraProjectId], issuetypeIds: [inputs.jiraTaskTypeId], expand: "."},
+    await jira_test.issues.getCreateIssueMetadata({projectIds: [inputs.jiraProjectId], issuetypeIds: [inputs.jiraTaskTypeId], expand: "fields"},
       (error: any, data: any) => {
         core.debug("Finished getting metadata")
         if (data) {
